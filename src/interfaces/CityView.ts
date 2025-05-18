db7b9aeacefedd77a0193f56b9a437c184a6d957
a.tuz86 <a.tuz86@gmail.com>
2025-05-18 18:50:11 -0500

    feat: add initial seed data

---
+++ src/interfaces/CityView.ts
@@ -0,0 +1,9 @@
+export interface CityView {
+  id: number;
+  city_id: number;
+  view_name: string;
+}
+
+export interface CityViewDTO {
+  view_name: string;
+}
---
+++ src/interfaces/Country.ts
@@ -0,0 +1,10 @@
+export interface Country {
+  id: number;
+  name: string;
+  iso_code: string;
+}
+
+export interface CountryDTO {
+  name: string;
+  iso_code: string;
+}
